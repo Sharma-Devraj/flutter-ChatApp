839c7ca06a8ad0b95e95205a380702e1d1e7dc73
Devraj Sharma <devraj20054@iiitd.ac.in>
2022-06-01 15:49:08 +0530

    demo websocket server

---
+++ server-websocket/index.js
@@ -0,0 +1,28 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import express from 'express';
+import expressWs from 'express-ws';
+import http from 'http'
+
+let port = 3000;
+let app = express();
+let server = http.createServer(app).listen(port);
+
+expressWs(app, server);
+
+app.use(express.static(__dirname+'/views'));
+app.get('/', (req,res) => {res.status(200).send("Welcome to my server")});
+
+app.ws('/ws', async function(ws, req) {
+    ws.on('message', async function(msg) {
+        console.log(msg);
+        ws.send(JSON.stringify({
+            "append" : true,
+            "returnText" : msg
+        }));
+    });
+});
